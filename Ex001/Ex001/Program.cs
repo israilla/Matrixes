ed0e779893558a404f6ebae1469afbd17feaf073
Israilla <israillacaroline@hotmail.com>
2020-05-17 17:31:33 -0300

    Exercise completed

---
+++ Ex001/Ex001/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Ex001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter matrix size: ");
+            string[] line = Console.ReadLine().Split(' ');
+            int n = int.Parse(line[0]);
+            int m = int.Parse(line[1]);
+
+            int[,] mat = new int[n, m];
+
+            Console.WriteLine("enter the numbers of the matrix positions");
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < m; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine("Enter with a number to localize the position: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (mat[i, j] == x)
+                    {
+                        Console.WriteLine("Position " + i + "," + j + ":");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mat[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mat[i - 1, j]);
+                        }
+                        if (j < m - 1)
+                        {
+                            Console.WriteLine("Right:" + mat[i, j + 1]);
+                        }
+                        if (i < n - 1)
+                        {
+                            Console.WriteLine("Down: " + mat[i + 1, j]);
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
