97a16593b35de863a4b292f3c140719069babf5d
Israilla <israillacaroline@hotmail.com>
2020-05-17 19:16:26 -0300

    Exercise 02 completed

---
+++ Ex002/Ex002/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Ex002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please, enter with of matrix size (enter only one number): ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            Console.WriteLine("Enter the matrix numbers: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine("Main diagonal: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Negative numbers: " + count);
+            Console.ReadKey();
+        }
+    }
+}
